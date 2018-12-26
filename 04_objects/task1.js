8652b962c708b3e747b841bcfa2fbdd87ff29404
ShmatEugene <shmat.evg@yandex.ru>
2018-12-26 17:25:14 +0300

    Reduce and slice

--- 04_objects/task1.js
+++ 04_objects/task1.js
@@ -34,8 +34,40 @@ Array.prototype.filterF = function (callback) {
   return newArray;
 }
 
+//reduce
+Array.prototype.reduceF = function(callback, startValue) {
+  let res = startValue;
+  for (var i = 0; i < this.length; i++) {
+    res = callback(res, this[i], i, this);
+  }
+  return res;
+}
+
+//slice
+Array.prototype.sliceF = function(sliceStart, sliceEnd) {
+  let newArray = [];
+
+  if(!sliceStart) {
+    sliceStart = 0;
+  }
+  if(!sliceEnd) {
+    sliceEnd = this.length;
+  }
+
+  if (sliceStart < 0) sliceStart = this.length + sliceStart;
+  if (sliceEnd < 0) sliceEnd = this.length + sliceEnd;
+
+  for (var i = 0; i < this.length; i++) {
+    if((i > sliceStart - 1) && (i <= sliceEnd - 1)) {
+      newArray[newArray.length] = this[i];
+    }
+  }
+
+  return newArray;
+}
+
 array1.forEachF(function(num){
-  console.log(++num);
+  //console.log(++num);
 });
 
 let array2 = array1.mapF(function(num, index, nums) {
@@ -46,5 +78,15 @@ let array3 = array2.filterF(function(num){
   return num > 5;
 });
 
-console.log(array2);
-console.log(array3);
+let array4 = array1.reduceF(function(startNum, num) {
+  return startNum + num;
+}, 1)
+
+let array5 = array1.sliceF(-4, -2);
+let array6 = array1.slice(-4, -2);
+
+//console.log(array2);
+//console.log(array3);
+//console.log(array4);
+console.log(array5);
+console.log(array6);
